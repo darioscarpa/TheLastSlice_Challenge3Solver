aeb6d95f1e80c73dc5dfce0df943142541579082
Dario Scarpa <dario.scarpa@gmail.com>
2018-08-20 20:39:08 +0200

    first (and probably last) commit
    Well,it works!

---
+++ LastSliceOven/LastSliceOven/OvenManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Windows.Storage.Streams;
+using Windows.Devices.Gpio;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+
+namespace LastSliceOven
+{
+    class OvenManager
+    {
+        // straight from the challenge e-mail!
+        private static readonly Dictionary<char, int> s_letterToPinMapping = new Dictionary<char, int>() {
+            {'A', 2 }, 
+            {'B', 3 }, 
+            {'D', 4 }, 
+            {'L', 5 }, 
+            {'N', 6 }, 
+            {'P', 7 }, 
+            {'R', 8 }, 
+            {'T', 9 }, 
+            {'U', 10 } 
+        }; 
+
+        public enum eBtn
+        {
+            UP,
+            DN,
+            LT,
+            RT,
+            A,
+            B
+        };
+
+        public enum eBtnState
+        {
+            PRESSED,
+            UNPRESSED
+        };
+
+        private const GpioPinValue BTN_PRESSED_VALUE = GpioPinValue.Low;
+        private const GpioPinValue BTN_UNPRESSED_VALUE = GpioPinValue.High;
+
+        private GpioController m_rGpioController;
+        private readonly Dictionary<eBtn, GpioPin[]> m_rBtnToPinsMapping;
+
+        public OvenManager()
+        {
+            m_rGpioController = GpioController.GetDefault();
+            if (m_rGpioController == null) {
+                Debug.WriteLine("GPIO: cannot use controller! ");
+                return;
+            }
+
+            Dictionary<char, GpioPin> s_rGpioPins = new Dictionary<char, GpioPin>();
+            foreach (var kv in s_letterToPinMapping) {
+                char cPinLetter = kv.Key;
+                int iPinNumber = kv.Value;
+                try {
+                    GpioPin rPin = m_rGpioController.OpenPin(iPinNumber);
+                    rPin.Write(BTN_UNPRESSED_VALUE);
+                    rPin.SetDriveMode(GpioPinDriveMode.Output);
+                    Debug.WriteLine("GPIO: opened pin " + iPinNumber + " in output mode");
+                    s_rGpioPins.Add(cPinLetter, rPin);
+                } catch (Exception e) {
+                    Debug.WriteLine("GPIO: cannot open pin " + iPinNumber);
+                    return;
+                }
+            }
+
+            m_rBtnToPinsMapping = new Dictionary<eBtn, GpioPin[]>() {
+                { eBtn.A, new GpioPin[] { s_rGpioPins['A'] }},
+                { eBtn.B, new GpioPin[] { s_rGpioPins['B'] }},
+                { eBtn.UP, new GpioPin[] { s_rGpioPins['U'], s_rGpioPins['P'] }},
+                { eBtn.DN, new GpioPin[] { s_rGpioPins['D'], s_rGpioPins['N'] }},
+                { eBtn.LT, new GpioPin[] { s_rGpioPins['L'], s_rGpioPins['T'] }},
+                { eBtn.RT, new GpioPin[] { s_rGpioPins['R'], s_rGpioPins['T'] }},
+             };
+
+            // part of me likes this alternative generic initialization too
+            //
+            //s_btnToPinsMapping = new Dictionary<eBtn, GpioPin[]>();
+            //foreach (eBtn btn in Enum.GetValues(typeof(eBtn))) {
+            //    List<GpioPin> list = new List<GpioPin>();
+            //    string btnName = btn.ToString();
+            //    for (int i = 0; i < btnName.Length; ++i) {
+            //        list.Add(s_rGpioPins[btnName[i]]);
+            //    }
+            //    s_btnToPinsMapping.Add(btn, list.ToArray());
+            //}
+        }
+
+
+        public void setBtn(eBtn btn, eBtnState state)
+        {
+            Debug.WriteLine("GPIO -> " + btn + " " + state);
+            GpioPinValue v = state == eBtnState.PRESSED ? BTN_PRESSED_VALUE : BTN_UNPRESSED_VALUE;
+            foreach (GpioPin rPin in m_rBtnToPinsMapping[btn]) {
+                rPin.Write(v);
+            }
+        }
+
+        // straight from the docs, nothing fancy!
+        public async Task<string> readFromUART()
+        {
+            string aqs = SerialDevice.GetDeviceSelector("UART0");
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+            SerialDevice SerialPort = await SerialDevice.FromIdAsync(dis[0].Id);
+            Debug.WriteLine("UART opened");
+
+            SerialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+            SerialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            SerialPort.BaudRate = 9600;
+            SerialPort.Parity = SerialParity.None;
+            SerialPort.StopBits = SerialStopBitCount.One;
+            SerialPort.DataBits = 8;
+            Debug.WriteLine("UART configured");
+
+            const uint maxReadLength = 1024;
+            DataReader dataReader = new DataReader(SerialPort.InputStream);
+            dataReader.InputStreamOptions = InputStreamOptions.Partial;
+            uint bytesToRead = await dataReader.LoadAsync(maxReadLength);
+            string rxBuffer = dataReader.ReadString(bytesToRead);
+
+            return rxBuffer;
+        }
+    }
+}
---
+++ LastSliceOven/LastSliceOven/StartupTask.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Windows.ApplicationModel.Background;
+using Windows.Networking.Sockets;
+
+namespace LastSliceOven
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+
+        // this is just to conveniently store a sequence of inputs
+        // chars aren't related to the pin mapping (abstracted by OvenManager)
+
+        private static readonly Dictionary<char, OvenManager.eBtn> s_rLetterToBtnMapping =
+            new Dictionary<char, OvenManager.eBtn>() {
+                {'A', OvenManager.eBtn.A },
+                {'B', OvenManager.eBtn.B } ,
+                {'D', OvenManager.eBtn.DN }, // DOWN
+                {'L', OvenManager.eBtn.LT }, // LEFT
+                {'R', OvenManager.eBtn.RT }, // RIGHT
+                {'U', OvenManager.eBtn.UP }  // UP
+            };
+
+        // the unlock sequence
+        const string KONAMI_CODE = "UUDDLRLRBA";
+
+        // start -> jalapeno
+        const string INGR_1 = "RRRD";
+
+        // jalapeno -> pepperoni
+        const string INGR_2 = "DLLDDDDDDR";
+
+        // pepperoni -> mushrooms
+        const string INGR_3 = "LUUUUUURRRRRRRRRR";
+
+        // mushrooms -> sausage
+        const string INGR_4 = "DDDRDDDL";
+
+        // delay settings (msec)
+        private const int iDELAY_AFTER_BTN_PRESS = 200;
+        private const int iDELAY_AFTER_BTN_RELEASE = 300;
+        private const int iDELAY_AFTER_INPUT_SEQ = 2000;
+
+        // handles the low level details
+        private OvenManager m_rOvenManager;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            m_rOvenManager = new OvenManager();
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            // used while trying to solve the challenge (remote input from pc)
+            //await allowRemoteControl();
+
+            // performs all the required steps to solve the challenge
+            await solveChallenge();
+
+            deferral.Complete();
+        }
+
+        private async Task sendInputSequence(string sInputSequence)
+        {
+            for (int i = 0; i < sInputSequence.Length; ++i) {
+                char cInputLetter = sInputSequence[i];
+                OvenManager.eBtn input = s_rLetterToBtnMapping[cInputLetter];
+                m_rOvenManager.setBtn(input, OvenManager.eBtnState.PRESSED);
+                await (Task.Delay(iDELAY_AFTER_BTN_PRESS));
+                m_rOvenManager.setBtn(input, OvenManager.eBtnState.UNPRESSED);
+                await (Task.Delay(iDELAY_AFTER_BTN_RELEASE));
+            }
+        }
+
+        private async Task solveChallenge()
+        {
+            string[] inputSequences = new string[] {
+                KONAMI_CODE, INGR_1, INGR_2, INGR_3, INGR_4
+            };
+            foreach (string sInputSeq in inputSequences) {
+                await sendInputSequence(sInputSeq);
+                await Task.Delay(iDELAY_AFTER_INPUT_SEQ);
+            }
+
+            string sFromUART = await m_rOvenManager.readFromUART();
+            Debug.WriteLine("UART <- " + sFromUART);
+        }
+
+
+        // ugly but somewhat functional
+        private async Task allowRemoteControl()
+        {
+            TaskCompletionSource<bool> canStopListening = new TaskCompletionSource<bool>();
+            try {
+                var streamSocketListener = new StreamSocketListener();
+                streamSocketListener.ConnectionReceived += async
+                    (StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args) => {
+                        Debug.WriteLine("TCP: connection received");
+                        string request;
+                        using (var streamReader = new StreamReader(args.Socket.InputStream.AsStreamForRead())) {
+                            while (true) {
+                                request = await streamReader.ReadLineAsync();
+                                Debug.WriteLine("TCP <- " + request);
+                                if (request.StartsWith("close")) {
+                                    canStopListening.SetResult(true);
+                                    args.Socket.Dispose();
+                                }
+                                if (request.StartsWith("serial")) {
+                                    string sFromUART = await m_rOvenManager.readFromUART();
+                                    Debug.WriteLine("UART <- " + sFromUART);
+                                } else if (request.StartsWith("konami")) {
+                                    await sendInputSequence(KONAMI_CODE); ;
+                                } else if (request.StartsWith("JP")) {
+                                    await sendInputSequence(INGR_1);
+                                } else if (request.StartsWith("PP")) {
+                                    await sendInputSequence(INGR_2);
+                                } else if (request.StartsWith("MR")) {
+                                    await sendInputSequence(INGR_3);
+                                } else if (request.StartsWith("SA")) {
+                                    await sendInputSequence(INGR_4);
+                                } else { // key press/release - request is one of [U|D|L|R][0|1]
+                                    OvenManager.eBtn btn = s_rLetterToBtnMapping[request[0]];
+                                    OvenManager.eBtnState btnState = request[1] == '0' ?
+                                        OvenManager.eBtnState.PRESSED : OvenManager.eBtnState.UNPRESSED;
+                                    m_rOvenManager.setBtn(btn, btnState);
+                                }
+                            }
+                        }
+                    };
+                await streamSocketListener.BindServiceNameAsync("12345");
+                Debug.WriteLine("TCP: waiting for connections");
+                await canStopListening.Task;
+                Debug.WriteLine("TCP: closing");
+                streamSocketListener.Dispose();
+            } catch (Exception ex) {
+                SocketErrorStatus webErrorStatus = SocketError.GetStatus(ex.GetBaseException().HResult);
+                Debug.WriteLine(webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message);
+            }
+        }
+    }
+}
